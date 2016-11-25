f1fff48747880aff8aabf06a05b10d2cabff120e
YesCT <cathy.theys@yestrainingandeducation.com>
2016-11-25 14:22:45 -0500

    Get ready for php

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+
+echo("<html><head><title>drupal</title></head><body>me</body></html>");
---
+++ tests/BioTest.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Tests for text from a bio.
+ */
+class BioTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+
+  protected function setUp()
+  {
+    $this->setBrowser('chrome');
+    $this->setBrowserUrl(PHPUNIT_TESTSUITE_EXTENSION_SELENIUM_BASEURL);
+  }
+
+  public function testBio()
+  {
+    $this->url('/index.php');
+    $this->assertEquals('drupal', $this->title());
+  }
+
+}
