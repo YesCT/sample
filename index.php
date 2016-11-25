ccfbe8741f293dd4bca7c1a672079807b3a2195a
YesCT <cathy.theys@yestrainingandeducation.com>
2016-11-25 15:11:30 -0500

    Add API wrapper
    Mentioned in https://iag.me/socialmedia/build-your-first-twitter-app-using-php-in-8-easy-steps/
    Cloned from https://github.com/J7mbo/twitter-api-php

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 
+// Wrapper referenced in
+// https://iag.me/socialmedia/build-your-first-twitter-app-using-php-in-8-easy-steps/
+//require_once('src/twitter-api-php/TwitterAPIExchange.php');
+
+/**
+ * Do not commit secrets to the git repo.
+ *
+ * Format like:
+ * @code
+ * const YOUR_OAUTH_ACCESS_TOKEN = 'xxxx';
+ * const YOUR_OAUTH_ACCESS_TOKEN_SECRET = 'xxxx';
+ * const YOUR_CONSUMER_KEY = 'xxxx';
+ * const YOUR_CONSUMER_SECRET = 'xxxx';
+ * @endcode
+ */
+require_once('mysecrets.php');
+
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+  'oauth_access_token' => YOUR_OAUTH_ACCESS_TOKEN,
+  'oauth_access_token_secret' => YOUR_OAUTH_ACCESS_TOKEN_SECRET,
+  'consumer_key' => YOUR_CONSUMER_KEY,
+  'consumer_secret' => YOUR_CONSUMER_SECRET,
+);
+
 echo("<html><head><title>drupal</title></head><body>");
 echo("me");
 echo("</body></html>");
