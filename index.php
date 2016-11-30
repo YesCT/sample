3db935249406fe3f83e6cee69174a8e56bae2d02
YesCT <cathy.theys@yestrainingandeducation.com>
2016-11-30 17:44:04 -0500

    Replace initial index with the simpler sample
    Nice. Integration tests pass.

--- index.php
+++ index.php
@@ -17,68 +17,27 @@ require __DIR__ . '/vendor/autoload.php';
  */
 require_once('mysecrets.php');
 
-$settings = array(
-  'oauth_access_token' => YOUR_OAUTH_ACCESS_TOKEN,
-  'oauth_access_token_secret' => YOUR_OAUTH_ACCESS_TOKEN_SECRET,
-  'consumer_key' => YOUR_CONSUMER_KEY,
-  'consumer_secret' => YOUR_CONSUMER_SECRET,
-);
-
 ?>
 
 <html><head><title>Cathy Test</title></head><body>
 
-<form name="twitterUsername" method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" >
-Username: <input type="text" name="user" value="" />
-<input type="submit" value="Gather Tweets" />
-</form>
-
 <?php
 
-// If the form has been submitted.
+$gatherer = new \CathyTest\Gatherer();
+
+$controller = new \CathyTest\GathererController($gatherer);
+
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-  $username_input = $_GET["user"];
-
-  $preprocess = new \CathyTest\Preprocess();
-  $user = $preprocess->preprocessUsername($username_input);
-
-  $validation = new \CathyTest\Validation();
-  $username_is_valid = $validation->isValidUsername($user);
-
-  // Check for non-empty and valid input.
-  if ($user != '') {
-    if (!$username_is_valid) {
-      echo("<h2>User Not Valid</h2>");
-      echo($user);
-    }
-    else {
-      echo("<h2>User</h2>");
-      echo($user);
-
-      // Get the results from twitter for the username from the form.
-      $something = new \CathyTest\JsonResult();
-      $result_json = $something->getJsonTimeline($settings, $user);
-
-      $result = new \CathyTest\Result();
-
-      // If no error, process the JSON result.
-      if (!$result->hasError($result_json)) {
-        $description = $result->getDescription($result_json);
-        $id = $result->getId($result_json);
-
-        // Print out select information.
-        echo("<h2>User description</h2>");
-        echo($description);
-
-        echo("<h2>Tweet id</h2>");
-        echo($id);
-      }
-
-      // Print out the raw JSON for debugging.
-      echo("<h2>Raw</h2>");
-      echo($result_json);
-    }
+  if(isset($_GET['user'])) {
+    $username_input = $_GET["user"];
+    $controller->gather($_GET);
   }
 }
 
-echo("</body></html>");
+$view = new \CathyTest\View($gatherer);
+
+echo $view->output();
+
+?>
+
+</body></html>
